2172013c7b4e9a8c4d4f64f9f2ffb5c12270115d
Md Rafi Akhtar <alimdrafi@gmail.com>
2020-08-13 00:52:03 +0530

    added test with debugElement

--- src/app/banner/banner-initial.component.spec.ts
+++ src/app/banner/banner-initial.component.spec.ts
@@ -1,5 +1,6 @@
 import { BannerComponent } from './banner-initial.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
 
 describe('Banner component (with beforeEach)', () => {
     let fixture: ComponentFixture<BannerComponent>;  // this will help the test interact with the template of the component
@@ -25,6 +26,15 @@ describe('Banner component (with beforeEach)', () => {
 
     it('should have <p> with "banner works!"', () => {
         const p = bannerElement.querySelector('p');
-        expect(p.textContent).toBe("banner works!");
+        expect(p.textContent).toBe('banner works!');
+    });
+
+    it('should have <p> with "banner works!" by debugElement', () => {
+        const bannerDe: DebugElement = fixture.debugElement;
+        const bannerEl: HTMLElement = bannerDe.nativeElement;
+        // in other words, the nativeElement can be fetched as fixture.debugElement.nativeElement
+
+        const p = bannerEl.querySelector('p');
+        expect(p.textContent).toBe('banner works!');
     });
 });
