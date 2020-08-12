b700fdf7fc8f638b135e353cffb7442b1e3da0c4
Md Rafi Akhtar <alimdrafi@gmail.com>
2020-08-13 00:44:58 +0530

    all nativeElement tests added for banner-initial component

--- src/app/banner/banner-initial.component.spec.ts
+++ src/app/banner/banner-initial.component.spec.ts
@@ -4,6 +4,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 describe('Banner component (with beforeEach)', () => {
     let fixture: ComponentFixture<BannerComponent>;  // this will help the test interact with the template of the component
     let component: BannerComponent;
+    let bannerElement: HTMLElement;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -11,14 +12,19 @@ describe('Banner component (with beforeEach)', () => {
         });
         fixture = TestBed.createComponent(BannerComponent);
         component = fixture.componentInstance;
+        bannerElement =  fixture.nativeElement;  // this will help interact with the DOM elements of the component
     });
 
     it('should be created', () => {
         expect(component).toBeDefined();
     });
 
-    it('should contain "banner works!', () => {
-        const bannerElement: HTMLElement = fixture.nativeElement;  // this will help interact with the DOM elements of the component
+    it('should contain "banner works!"', () => {
         expect(bannerElement.textContent).toBe('banner works!');
+    });
+
+    it('should have <p> with "banner works!"', () => {
+        const p = bannerElement.querySelector('p');
+        expect(p.textContent).toBe("banner works!");
     });
 });
