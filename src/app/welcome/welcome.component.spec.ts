a9ffc75005e835f6ece9aab0b2f4afb4cb314222
Md Rafi Akhtar <alimdrafi@gmail.com>
2020-08-15 21:42:28 +0530

    used stubs as a test double for testing service dependency

--- src/app/welcome/welcome.component.spec.ts
+++ src/app/welcome/welcome.component.spec.ts
@@ -1,5 +1,5 @@
 import { WelcomeComponent } from './welcome.component';
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { UserService } from '../model/user.service';
 
 /** The welcome component has a dependency - UserService component.
@@ -9,6 +9,7 @@ import { UserService } from '../model/user.service';
 
 // In order to solve this dependency, first create a mock class that mimics the minimum requirement of UserService
 class MockUserService {
+    // Note: Doubles like mocks are only used as a useClass in TestBed provider; everywhere else, use the injected service
     public isLoggedIn = true;
     public user = { name: 'Mock user' };
 }
@@ -43,5 +44,60 @@ describe('Welcome component (class only)', () => {
         comp.ngOnInit();
         expect(comp.welcome).not.toContain(userService.user.name);
         expect(comp.welcome).toContain('log in');
+    });
+});
+
+describe('Welcome component', () => {
+    let userServiceStub: Partial<UserService>;
+    let fixture: ComponentFixture<WelcomeComponent>;
+    let comp: WelcomeComponent;
+    let userService: UserService;
+    let el: HTMLElement;
+
+    beforeEach(() => {
+        userServiceStub = {
+            // Note: Doubles like stubs are only used as useValue in TestBed provider; everywhere else, use the injected service
+            isLoggedIn: true,
+            user: { name: 'Test User' }
+        }
+
+        TestBed.configureTestingModule({
+            declarations: [WelcomeComponent],
+            providers: [{
+                provide: UserService,
+                useValue: userServiceStub
+            }]
+        });
+
+        fixture = TestBed.createComponent(WelcomeComponent);
+        comp = fixture.componentInstance;
+        userService = fixture.debugElement.injector.get(UserService);
+        /* Alternatively,
+            userService = TestBed.inject(UserService);
+        But, this will work only when the only provider of the service is the root testing module (which is true in this case)
+        But the injector from the component's fixture will always work.
+        */
+
+        el = fixture.nativeElement.querySelector('.welcome');
+    });
+
+    it('should welcome the user', () => {
+        fixture.detectChanges();
+        const greeting = el.textContent;
+        expect(greeting).toContain('Test User');
+        expect(greeting).toContain('Welcome');
+    });
+
+    it('should welcome "Rafi"', () => {
+        userService.user.name = 'Rafi';
+        fixture.detectChanges();
+        expect(el.textContent).toContain('Rafi');
+    });
+
+    it('should request log in if not logged in', () => {
+        userService.isLoggedIn = false;
+        fixture.detectChanges();
+        expect(el.textContent).not.toContain('Welcome');
+        expect(el.textContent).toContain('log in');
     });
 });
