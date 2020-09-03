2eac996a50d76e9ada25f293905c4f7752ba90dc
Md Rafi Akhtar <alimdrafi@gmail.com>
2020-09-03 23:17:15 +0530

    setup dashboardherocomponent testing directly and added a test

--- src/app/dashboard/dashboard-hero.component.spec.ts
+++ src/app/dashboard/dashboard-hero.component.spec.ts
@@ -1,5 +1,8 @@
 import { DashboardHeroComponent } from './dashboard-hero.component';
 import { Hero } from '../model/hero';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('DashboardHeroComponent (class only)', () => {
     it('raises the selected event when clicked', () => {
@@ -11,5 +14,35 @@ describe('DashboardHeroComponent (class only)', () => {
             expect(selectedHero).toEqual(comp.hero);
         });
         comp.click();
+    });
+});
+
+describe('DashboardHeroComponent when tested directly', () => {
+    let comp: DashboardHeroComponent;
+    let fixture: ComponentFixture<DashboardHeroComponent>;
+    let heroEl: HTMLElement;
+    let heroDe: DebugElement;
+    let expectedHero: Hero;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [ DashboardHeroComponent ]
+        });
+
+        fixture = TestBed.createComponent(DashboardHeroComponent);
+        comp = fixture.componentInstance;
+
+        heroDe = fixture.debugElement.query(By.css('.hero'));
+        heroEl = heroDe.nativeElement;
+
+        expectedHero = {id: 42, name: 'Tes name'};
+        comp.hero = expectedHero;
+        
+        fixture.detectChanges();
+    });
+
+    it('should display hero name in upper case', () => {
+        const expectedPipedName = expectedHero.name.toUpperCase();
+        expect(heroEl.textContent).toContain(expectedPipedName);
     });
 });
