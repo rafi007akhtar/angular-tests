9798159be7c94f5cd27c2e3ee60558c016a52176
Md Rafi Akhtar <alimdrafi@gmail.com>
2020-09-20 19:26:14 +0530

    A couple of changes regarding hero-detail.component.spec.ts
    - fixed the beforeEach and provided all the needed stubs
    - made sure fixture detects changes on async using whenStable's promise
    - the title test case now works - the pending work from so long is
      finally in place

--- src/app/hero/hero-detail.component.spec.ts
+++ src/app/hero/hero-detail.component.spec.ts
@@ -1,6 +1,6 @@
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroDetailService } from './hero-detail.service';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 import { TestHeroService } from '../model/testing/test-hero.service';
 import { HeroModule } from './hero.module';
 import { ActivatedRouteStub, ActivatedRoute } from '../../testing/activated-route-stub';
@@ -13,41 +13,46 @@ function newEvent(eventName: string, bubbles = false, cancelable = false) {
     return evt;
 }
 
-// stubs
-const activatedStub: any = {}
-
 describe('when navigates to existing hero', () => {
     let fixture: ComponentFixture<HeroDetailComponent>;
     let component: HeroDetailComponent;
     const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    const activatedRouteStub = new ActivatedRouteStub()
 
-    beforeEach(() => {
+    beforeEach(fakeAsync(() => {
         TestBed.configureTestingModule({
             imports: [ HeroModule ],
-            declarations: [HeroDetailComponent],
+            declarations: [ HeroDetailComponent ],
             providers: [
-                { provide: HeroDetailService, useClass: TestHeroService },
-                { provide: ActivatedRoute, useValue: activatedStub },
-                { provide: HeroService, useValue: {} },
+                { provide: ActivatedRoute, useValue: activatedRouteStub },
+                { provide: HeroService, useValue: TestHeroService },
                 { provide: Router, useValue: routerSpy }
             ]
         });
+        createComponent();
+    }));
+
+    function createComponent() {
         fixture = TestBed.createComponent(HeroDetailComponent);
         component = fixture.componentInstance;
-    });
-    
+
+        fixture.detectChanges();  // ngOnInit
+
+        return fixture.whenStable().then(() => {
+            fixture.detectChanges();  // async calls
+        })
+    }
+
     it('should convert hero name to Title Case', () => {
-        // const hostElement = fixture.nativeElement;
-        // const input: HTMLInputElement = hostElement.querySelector('input');
-        // const name: HTMLElement = hostElement.querySelector('span');
+        const hostElement = fixture.nativeElement;
+        const input: HTMLInputElement = hostElement.querySelector('input');
+        const name: HTMLElement = hostElement.querySelector('span');
 
-        // input.value = 'some generic hero';  // simulate an input change
-        // input.dispatchEvent(newEvent('input'));  // tell Angular an input change has been simulated
+        input.value = 'some generic hero';  // simulate an input change
+        input.dispatchEvent(newEvent('input'));  // tell Angular an input change has been simulated
 
-        // fixture.detectChanges();
+        fixture.detectChanges();
 
-        // expect(name.textContent).toBe('Some Generic Hero');
-
-        pending('TestBed incomplete; will complete later and run again');
+        expect(name.textContent).toBe('Some Generic Hero');
     });
 });
