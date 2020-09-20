a4ff1cf4b634910bcff7801dfe40ed9d81dbc04f
Md Rafi Akhtar <alimdrafi@gmail.com>
2020-09-20 20:37:32 +0530

    A couple of changes in hero-detail.component.spec.ts
    - Restructured the file: now it has 3 main describes
    - The first one is for navigating to existing hero, second one for no
      hero id, and third for non-existent hero
    - Done with the first two
    - Third is remaining since it depends on custom Page class, which I
      haven't come across yet

--- src/app/hero/hero-detail.component.spec.ts
+++ src/app/hero/hero-detail.component.spec.ts
@@ -1,11 +1,12 @@
 import { HeroDetailComponent } from './hero-detail.component';
-import { HeroDetailService } from './hero-detail.service';
 import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
-import { TestHeroService } from '../model/testing/test-hero.service';
+import { Hero, TestHeroService, getTestHeroes } from '../model/testing/test-hero.service';
 import { HeroModule } from './hero.module';
 import { ActivatedRouteStub, ActivatedRoute } from '../../testing/activated-route-stub';
 import { HeroService } from '../model/hero.service';
 import { Router } from '@angular/router';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 function newEvent(eventName: string, bubbles = false, cancelable = false) {
     const evt = document.createEvent('CustomEvent');  // MUST be 'CustomEvent'
@@ -13,22 +14,27 @@ function newEvent(eventName: string, bubbles = false, cancelable = false) {
     return evt;
 }
 
-describe('when navigates to existing hero', () => {
+describe('HeroDetailComponent - when navigates to existing hero', () => {
     let fixture: ComponentFixture<HeroDetailComponent>;
     let component: HeroDetailComponent;
     const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
     const activatedRouteStub = new ActivatedRouteStub()
 
+    let expectedHero: Hero;
+
     beforeEach(fakeAsync(() => {
         TestBed.configureTestingModule({
-            imports: [ HeroModule ],
-            declarations: [ HeroDetailComponent ],
+            imports: [HeroModule],
+            declarations: [HeroDetailComponent],
             providers: [
                 { provide: ActivatedRoute, useValue: activatedRouteStub },
-                { provide: HeroService, useValue: TestHeroService },
+                { provide: HeroService, useClass: TestHeroService },
                 { provide: Router, useValue: routerSpy }
             ]
         });
+
+        expectedHero = getTestHeroes()[0];  // first hero
+        activatedRouteStub.setParamMap({id: expectedHero.id});
         createComponent();
     }));
 
@@ -54,5 +60,83 @@ describe('when navigates to existing hero', () => {
         fixture.detectChanges();
 
         expect(name.textContent).toBe('Some Generic Hero');
+    });
+});
+
+describe('HeroDetailComponent - when navigates with no hero id', () => {
+    let fixture: ComponentFixture<HeroDetailComponent>;
+    let component: HeroDetailComponent;
+    const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    const activatedRouteStub = new ActivatedRouteStub()
+
+    beforeEach(fakeAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [HeroModule],
+            declarations: [HeroDetailComponent],
+            providers: [
+                { provide: ActivatedRoute, useValue: activatedRouteStub },
+                { provide: HeroService, useClass: TestHeroService },
+                { provide: Router, useValue: routerSpy }
+            ]
+        });
+        createComponent();
+    }));
+
+    function createComponent() {
+        fixture = TestBed.createComponent(HeroDetailComponent);
+        component = fixture.componentInstance;
+
+        fixture.detectChanges();  // ngOnInit
+
+        return fixture.whenStable().then(() => {
+            fixture.detectChanges();  // async calls
+        })
+    }
+
+    it('should have hero.id to be 0', () => {
+        expect(component.hero.id).toBe(0);
+    });
+
+    it('should display empty hero name', () => {
+        const nameDe: DebugElement = fixture.debugElement.query(By.css('span'));
+        const nameEl: HTMLElement = nameDe.nativeElement;
+        expect(nameEl.textContent).toBe('')
+    });
+});
+
+describe('HeroDetailComponent - when navigates to non-existent hero', () => {
+    let fixture: ComponentFixture<HeroDetailComponent>;
+    let component: HeroDetailComponent;
+    const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    const activatedRouteStub = new ActivatedRouteStub();
+
+    beforeEach(fakeAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [HeroModule],
+            declarations: [HeroDetailComponent],
+            providers: [
+                { provide: ActivatedRoute, useValue: activatedRouteStub },
+                { provide: HeroService, useClass: TestHeroService },
+                { provide: Router, useValue: routerSpy }
+            ]
+        });
+
+        activatedRouteStub.setParamMap({id: 99999});
+        createComponent();
+    }));
+
+    function createComponent() {
+        fixture = TestBed.createComponent(HeroDetailComponent);
+        component = fixture.componentInstance;
+
+        fixture.detectChanges();  // ngOnInit
+
+        return fixture.whenStable().then(() => {
+            fixture.detectChanges();  // async calls
+        })
+    }
+
+    it('should try to navigate back to hero list', () => {
+        pending('will complete writing this after learning about the Page class in future');
     });
 });
